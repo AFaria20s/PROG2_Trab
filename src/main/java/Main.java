c347a13ab283ddba3fa33d0064399d93f89f7403
DevFlavioFernandes <dev.flaviofernandes@gmail.com>
2025-11-27 13:54:29 +0000

    Alteração nova classe teste

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("O afonso é guei");
+        System.out.println("O afonso é guei adasdasdasdasads");
     }
 }
