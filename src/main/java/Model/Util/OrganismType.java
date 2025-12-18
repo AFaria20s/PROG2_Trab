bc4e965d1359001391822eb107fe427af7566e0c
AfonsoFaria20s <affaria188@gmail.com>
2025-12-18 16:58:09 +0000

    Adicionada cor aos organismos

--- src/main/java/Model/Util/OrganismType.java
+++ src/main/java/Model/Util/OrganismType.java
@@ -1,26 +1,25 @@
 package Model.Util;
 
+import java.awt.Color;
+
 public enum OrganismType {
-    HUNTER("X", "Hunter"),
-    PLANT("*", "Plant"),
-    SHEEP("O", "Sheep"),
-    WOLF("w", "Wolf"),
-    EMPTY(".", "Empty")
-    ;
+    EMPTY(".", "Empty", new Color(255,255,255)),    // Branco
+    HUNTER("X", "Hunter", new Color(255, 69, 0)),   // Laranja avermelhado
+    PLANT("*", "Plant", new Color(34, 139, 34)),    // Verde Floresta
+    SHEEP("O", "Sheep", new Color(100, 149, 237)),  // Azul
+    WOLF("w", "Wolf", new Color(60, 60, 60));       // Cinzento Escuro
 
-    final String symbol;
-    final String type;
+    private final String symbol;
+    private final String type;
+    private final Color color;
 
-    OrganismType(String symbol, String type) {
+    OrganismType(String symbol, String type, Color color) {
         this.symbol = symbol;
         this.type = type;
+        this.color = color;
     }
 
-    public String getSymbol() {
-        return this.symbol;
-    }
-
-    public String asString() {
-        return this.type;
-    }
+    public String getSymbol() { return symbol; }
+    public String asString() { return type; }
+    public Color getColor() { return color; }
 }
